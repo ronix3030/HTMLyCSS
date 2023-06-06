e31c714cf128f111ccecd9b497c2811c24468aa4
ronix3030 <ronald3019@hotmail.com>
2023-06-05 19:03:46 -0500

    web 19

---
+++ JavaScrip/Main.js
@@ -0,0 +1,65 @@
+// Primer programa en javascript
+console.log("Hola Mundo"); 
+
+//VARIABLES
+var variable1 = 5; // tipó entero
+var variable2 = 24.5; // tipo decimales
+var variable3 ="Hola"; // tipo texto
+var variable4 = true; // tipo boleano 
+const  miconstante =5;  // tipo const no se puede modificar 
+console.log(variable1);
+console.log("Mi edad es" + variable2);
+// OPERACIONES
+var result =5+5; // todo tipo de oracion
+console.log(result);
+// otro ejemplo:
+var uno=3;
+var dos=4;
+var resul=uno+dos;
+console.log(resul);
+// CONDICIONALES
+// ejemplo1;
+let minumero=6;
+
+if  (minumero==6) {
+    console.log("Si es igual");
+}
+else{
+    console.log("No es igual");
+}
+// ejemplo2;
+// el operador && evalúa si ambas condiciones son verdaderas.
+let minumero2=5;
+let miletra2="Hola";
+if (minumero2==5 && miletra2=="Hola"){
+    console.log ("Es verdadero");   
+}
+else{
+    console.log("No es Verdadero");
+}
+// ejemplo3;
+//operador || evalúa si al menos una de las condiciones es verdadera, se ejecuta
+let vari1=10;
+let vari2=20;
+if(vari1<=10 || vari2<15){
+    console.log("Se cumple");
+}
+else{
+    console.log("No se cumple");
+        
+    }
+// ejemplo4:
+let ol=100;
+if(ol<100){
+    let qw=50; let au=50;
+    let result=qw+au;
+    console.log("la suma es"+result);
+}
+else if(ol>=result){
+    console.log("El resultado es mayor o igual a 100");
+}
+else{
+    console.log()
+}
+
+
